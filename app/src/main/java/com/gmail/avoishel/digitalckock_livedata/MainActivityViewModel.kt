ebab8a82db327653001561d16fe90e9a629ca3aa
Alexandr Voishel <avoishel@gmail.com>
2022-04-17 00:15:45 +0300

    add liveData for two digits and count logic

--- app/src/main/java/com/gmail/avoishel/digitalckock_livedata/MainActivityViewModel.kt
+++ app/src/main/java/com/gmail/avoishel/digitalckock_livedata/MainActivityViewModel.kt
@@ -11,67 +11,19 @@ import java.util.*
 
 class MainActivityViewModel: ViewModel() {
 
-    //region LiveData
-    private val _segmentTopLiveData = MutableLiveData(R.color.unselected)
-    val segmentTopLiveData: LiveData<Int> = _segmentTopLiveData
+    val secondRightDisplayManager = DigitsDisplayManager()
+    val secondLeftDisplayManager = DigitsDisplayManager()
 
-    private val _segmentTopLeftLiveData = MutableLiveData(R.color.unselected)
-    val segmentTopLeftLiveData: LiveData<Int> = _segmentTopLeftLiveData
-
-    private val _segmentTopRightLiveData = MutableLiveData(R.color.unselected)
-    val segmentTopRightLiveData: LiveData<Int> = _segmentTopRightLiveData
-
-    private val _segmentMiddleLiveData = MutableLiveData(R.color.unselected)
-    val segmentMiddleLiveData: LiveData<Int> = _segmentMiddleLiveData
-
-    private val _segmentBottomLeftLiveData = MutableLiveData(R.color.unselected)
-    val segmentBottomLeftLiveData: LiveData<Int> = _segmentBottomLeftLiveData
-
-    private val _segmentBottomRightLiveData = MutableLiveData(R.color.unselected)
-    val segmentBottomRightLiveData: LiveData<Int> = _segmentBottomRightLiveData
-
-    private val _segmentBottomLiveData = MutableLiveData(R.color.unselected)
-    val segmentBottomLiveData: LiveData<Int> = _segmentBottomLiveData
-    //endregion LiveData
-
-    private val liveDateList = listOf(
-        _segmentTopLiveData,
-        _segmentTopLeftLiveData,
-        _segmentTopRightLiveData,
-        _segmentMiddleLiveData,
-        _segmentBottomLeftLiveData,
-        _segmentBottomRightLiveData,
-        _segmentBottomLiveData
-    )
-
-    private val digitalMap = mapOf(
-        _segmentTopLiveData         to listOf(0, 2, 3, 5, 6, 7, 8, 9),
-        _segmentTopLeftLiveData     to listOf(0, 4, 5, 6,  8, 9),
-        _segmentTopRightLiveData    to listOf(0, 1, 2, 3, 4, 7, 8, 9),
-        _segmentMiddleLiveData      to listOf(2, 3, 4, 5, 6, 8, 9),
-        _segmentBottomLeftLiveData  to listOf(0, 2, 6, 8),
-        _segmentBottomRightLiveData to listOf(0, 1, 3, 4, 5, 6, 7, 8, 9),
-        _segmentBottomLiveData      to listOf(0, 2, 3, 5, 6, 8),
-    )
-
-    fun startCounting() {
+    fun startCounting(){
         viewModelScope.launch {
-            repeat(10){
-                onNewDigit(it)
-                delay(1_000)
+            var index = 0
+            while (true) {
+                secondRightDisplayManager.onNewDigit(index % 10)
+                secondLeftDisplayManager.onNewDigit((index / 10) % 10)
+                index++
+                delay(1_00)
             }
         }
     }
 
-    fun onNewDigit(digit: Int) {
-        liveDateList.forEach { segmentLiveDate ->
-            val colorRes = if (digitalMap[segmentLiveDate]!!.contains(digit)) {
-                R.color.selected
-            } else {
-                R.color.unselected
-            }
-
-            segmentLiveDate.postValue(colorRes)
-        }
-    }
 }
